781dbb4ba9c183cbfadf09929945d316fb2363c1
Yashgupta9330 <yaahg342@gmail.com>
2023-10-28 19:01:36 +0530

    changes in rightsidebar

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -1,6 +1,24 @@
 import React from "react";
-
+import Link from "next/link";
+import Image from "next/image";
+import RenderTag from "./RenderTag";
 const RightSideBar = () => {
+
+    const hotQuestions=[
+     {_id:1,title:'How i use expressjs as a custom scrollbar?'},
+     {_id:1,title:'How i use expressjs as a custom scrollbar?'},
+     {_id:1,title:'How i use expressjs as a custom scrollbar?'},
+     {_id:1,title:'How i use expressjs as a custom scrollbar?'},
+     {_id:1,title:'How i use expressjs as a custom scrollbar?'}
+    ];
+
+    const popularTags=[
+        {_id:1,name:'vue',numberOfQuestions:2},
+        {_id:1,name:'vue',numberOfQuestions:2},
+        {_id:1,name:'vue',numberOfQuestions:2},
+        {_id:1,name:'vue',numberOfQuestions:2},
+        {_id:1,name:'vue',numberOfQuestions:2}
+    ];
   return (
     <section
       className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen 
@@ -8,7 +26,45 @@ const RightSideBar = () => {
     shadow-light-300 dark:shadow-none 
     max-xl:hidden w-[350px]"
     >
-      RightSideBar
+     <div>
+        <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+        <div className="mt-7 flex w-full flex-col gap-[30px]">
+          {hotQuestions?.map((question) => (
+            <Link
+              href={`/question/${question._id}`}
+              key={question._id}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">
+                {question.title}
+              </p>
+
+              <Image
+                src="/assets/icons/chevron-right.svg"
+                alt="chevron-right"
+                width={20}
+                height={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.numberOfQuestions}
+              showCount
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
